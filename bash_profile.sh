81e4293952d1ced514bf4cd2c0f41b9c5821a1cf
inksea <oebudde13@gmail.com>
2020-03-08 10:56:25 -0400

    fix git status

--- bash_profile.sh
+++ bash_profile.sh
@@ -76,31 +76,26 @@ fi
 }
 
 # Parse Git Branch.
-parse_git_branch() {
+check_branch() {
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-# Set the prompt.
-if [ "$color_prompt" = yes ]; then
-	PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")[$(if [[ ${EUID} == 0 ]];
-		then echo '\[\033[01;31m\]root\[\033[01;33m\]\[\033[01;96m\]$(parse_git_branch)\';
-		else echo '\[\033[0;39m\]\u\[\033[01;33m\]\[\033[01;96m\]'; fi)\[\033[0;31m\]]\342\224\200[\[\033[0;32m\]\w\[\033[0;31m\]]\[\033[01;96m\]$(parse_git_branch)\[\033[0;31m\]$(check_status)\n\[\033[0;31m\]\342\224\224\342\224\200\342\224\200\342\225\274 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\]"
-else
-	PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")[$(if [[ ${EUID} == 0 ]];
-		then echo '\[\033[01;31m\]root\[\033[01;33m\]$(check_status)\[\033[01;96m\]$(parse_git_branch)';
-		else echo '\[\033[0;39m\]\u\[\033[01;33m\]$(check_status)\[\033[01;96m\]$(parse_git_branch)'; fi)\[\033[0;31m\]]\342\224\200[\[\033[0;32m\]\w\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\342\224\200\342\224\200\342\225\274 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\]"
-fi
+export PS1="\[\033[0;31m\]\342\224\214\342\224\200" # Left Branch.
+	PS1+="[\[\033[0;39m\]\u\[\033[01;33m\]@\[\033[01;96m\]\h" # User and system.
+	PS1+="\[\033[0;31m\]]\342\224\200[\[\033[0;32m\]\W\[\033[0;31m\]]" # Path.
+	PS1+="\[\033[01;96m\]\$(check_branch)\[\033[0;31m\]\$(check_status) \[$(tput sgr0)\]" # Git status.
+	PS1+="\n\[\033[0;31m\]\342\224\224\342\224\200\342\224\200\342\225\274 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\] " # Command line.
 
 unset color_prompt force_color_prompt
 
 # If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-	PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-	;;
-*)
-	;;
-esac
+# case "$TERM" in
+# xterm*|rxvt*)
+# 	PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+# 	;;
+# *)
+# 	;;
+# esac
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
